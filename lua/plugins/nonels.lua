7391d3440ac1c12bf92939754c831c3b5a2e7a0a
gdhillon2 <gman.dhillon@gmail.com>
2024-08-04 05:34:58 -0700

    added workaround for getting 4 space tabs on autoformat

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -1,18 +1,27 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.completion.spell,
-			},
-		})
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.prettier,
+                -- Add other sources if needed
+            },
+        })
+
+        -- Custom function to format and then indent
+        local function format_and_indent()
+            vim.lsp.buf.format({ async = true }) -- Format the buffer asynchronously
+            vim.defer_fn(function()
+                vim.cmd('normal! gg=G')          -- Auto-indent the entire file
+            end, 100)                            -- Delay to ensure formatting completes
+        end
+
+        -- Set up the keymap
+        vim.keymap.set("n", "<leader>gf", format_and_indent, {})
+    end,
 }
